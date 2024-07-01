df3f4f70c7e8ad39e33317ea2d870edc1b1a7747
ashesbloom <mayankpandeydk123@gmail.com>
2024-07-01 23:45:07 +0530

    echo

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
 import sys
 
+def echo(cmd):
+    sys.stdout.write(cmd.split('echo ')[1] + '\n')
+
+
+def exiting(cmd):
+    return exit(0)
+
+
+commands = {'echo': echo, 'exit 0': exiting}
+
+
+def execute(cmd):
+    for extract in commands:
+        if cmd.startswith(extract):
+            commands[extract](cmd)
+            return
+
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        # Wait for user input
         cmd = input()
 
-        if cmd.startswith("exit"):
-            exit(0)
+        if any(cmd.startswith(x)for x in commands.keys()):
+            execute(cmd)
         else:
             sys.stdout.write(f'{cmd}: command not found\n')
 
